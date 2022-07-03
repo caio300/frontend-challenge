487d4155da245e567c99270c188306d87ba05f4e
Caio <caioqs.info@gmail.com>
2022-07-02 21:19:00 -0300

    Implementando parte inicial paginaçao

--- src/Pages/Loja/Loja.js
+++ src/Pages/Loja/Loja.js
@@ -1,10 +1,18 @@
 import styled from "styled-components";
+
 export const PageLoja = styled.main`
   display: grid;
   grid-template-columns: 18rem 1fr;
   margin-left: 20rem;
   margin-right: 10rem;
 
+  .selected-page {
+    background: orange;
+  }
+
+  .button-1 {
+
+  }
   
   @media screen and (max-width: 800px) {
     margin-left: 1.125rem;
--- src/Pages/Loja/index.jsx
+++ src/Pages/Loja/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Catalogo from '../../components/Catalogo';
 import productsWine from '../../components/context/Context';
 import Header from '../../components/Header';
@@ -6,11 +6,26 @@ import Sidebar from '../../components/Sidebar';
 import * as Container from './Loja';
 
 export default function() {
-  const {  fetchWines } = useContext(productsWine);
-  
+  const {  fetchWines, page, setPage } = useContext(productsWine);
+  const [ css, setCss ] = useState('')
   useEffect(() => {
     fetchWines();
   }, []);
+
+  useEffect(() => {
+    fetchWines(page);
+  }, [page]);
+
+  const changePage = (target) => {
+    setPage(target.innerText);
+    
+  };
+
+  const printButton = (target) => {
+    if(Number(target.innerText) === page) {
+      target.className += ' selected-page';
+    }
+  };
 
   return (
     <>
@@ -18,6 +33,13 @@ export default function() {
       <Container.PageLoja>
         <Sidebar />
         <Catalogo />
+        <div>
+          <button onClick={({target}) => {changePage(target), printButton(target)}} className='button-1'>{page === 1 ? page : page - 1}</button>
+          <button onClick={({target}) => {changePage(target), printButton(target)}} className='button-2'>{page === 1 ? page + 1 :page}</button>
+          <button onClick={({target}) => {changePage(target), printButton(target)}} className='button-3'>{page === 1 ? page + 2 :page + 1}</button>
+          <span>...</span>
+          <button onClick={() => setPage(page + 1)} className='button-next'>Próximo</button>
+        </div>
       </Container.PageLoja>
     </>
   )
